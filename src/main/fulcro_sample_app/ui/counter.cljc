73dff85d053cb7a65a29a6ca9be574046b3e71e7
ponkore <ponkore@gmail.com>
2018-12-08 21:21:03 +0900

    add counter sample component

---
+++ src/main/fulcro_sample_app/ui/counter.cljc
@@ -0,0 +1,23 @@
+(ns fulcro-sample-app.ui.counter
+  (:require
+   [fulcro.client.primitives :as prim :refer [defsc]]
+   [fulcro.client.mutations :refer [defmutation]]
+   #?(:cljs [fulcro.client.dom :as dom] :clj [fulcro.client.dom-server :as dom])
+   [sablono.core :refer-macros [html]]))
+
+(defmutation bump-number [ignored]
+  (action [{:keys [state]}]
+    (swap! state update :counter/cnt inc)))
+
+(defsc Counter
+  "simple counter example component"
+  [this {:keys [counter/cnt]}]
+  {:query         [:counter/cnt]
+   :initial-state {:counter/cnt 0}}
+  (html
+   [:div
+    [:h4 "This is an example"]
+    [:button {:on-click #(prim/transact! this `[(bump-number {})])}
+     "You've clicked this button " cnt " times."]]))
+
+(def ui-counter (prim/factory Counter))
--- src/main/fulcro_sample_app/ui/root.cljs
+++ src/main/fulcro_sample_app/ui/root.cljs
@@ -3,11 +3,12 @@
     [fulcro.client.dom :as dom :refer [div]]
     [fulcro.client.primitives :as prim :refer [defsc]]
     [fulcro-sample-app.ui.components :as comp]
+    [fulcro-sample-app.ui.counter :as counter]
     [sablono.core :refer-macros [html]]))
 
-(defsc Root [this {:keys [root/message]}]
-  {:query         [:root/message]
-   :initial-state {:root/message "Hello!"}}
+(defsc Root [this {:keys [root/message counter/cnt]}]
+  {:query         [:root/message :counter/cnt]
+   :initial-state {:root/message "Hello!" :counter/cnt 0}}
   (html
    [:div.ui.segments
     [:div.ui.top.attached.segment
@@ -16,5 +17,6 @@
     [:div.ui.attached.segment
      [:div.content
       (comp/ui-placeholder {:w 50 :h 50})
+      (counter/ui-counter {:counter/cnt cnt})
       [:div message]
       [:div "Some content here would be nice!!!"]]]]))
